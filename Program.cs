7fe4e786e975746078bb34b0565b896e0275db58
Илья Алексеев <av98060@gmail.com>
2022-12-19 21:50:28 +0300

    New

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿int [] createMas(int sizeMas)
+{
+    int [] mas= new int [sizeMas];
+    Random rs=new Random();
+    for (int i=0; i<sizeMas; i++)
+    {
+        mas[i]=rs.Next(100, 999);
+        if (i<sizeMas-1)
+        {
+            Console.Write($"{mas[i]}, ");
+        } else
+        {
+            Console.Write($"{mas[i]}");
+            Console.WriteLine();
+        }
+    }
+    return mas;
+}
+
+int numChet(int [] mas)
+{
+    int num=0;
+    for (int i=0; i<mas.Length; i++)
+    {
+        if (mas[i]%2==0)
+        {
+            num=++num;
+        }
+    }
+    return num;
+}
+Console.WriteLine("Введите размер массива трехзначных чисел");
+int [] masiv=createMas(int.Parse(Console.ReadLine()));
+Console.WriteLine($"{numChet(masiv)}");
